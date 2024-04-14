9dce43b7e215875f2aad920f52a017cf69e25b76
SubjectQuit <103890959+SubjectQuit@users.noreply.github.com>
2024-04-14 23:35:57 +0200

    Added support for attitude estimation

--- src/IMU.h
+++ src/IMU.h
@@ -31,15 +31,6 @@ private:
     #define GYRO_SCALE_FACTOR 131.0
 
     //IMU:
-    float AccX, AccY, AccZ;
-    float AccX_prev, AccY_prev, AccZ_prev;
-    float GyroX, GyroY, GyroZ;
-    float GyroX_prev, GyroY_prev, GyroZ_prev;
-    float q0 = 1.0f; //Initialize quaternion for madgwick filter
-    float q1 = 0.0f;
-    float q2 = 0.0f;
-    float q3 = 0.0f;
-
 
     const float AccErrorX = 0.00;
     const float AccErrorY = -0.01;
@@ -50,13 +41,27 @@ private:
     const float GyroErrorY = 0.64;
     const float GyroErrorZ = -0.97;
 
-    const float B_accel = 0.14;     //Accelerometer LP filter paramter, (MPU6050 default: 0.14. MPU9250 default: 0.2)
-    const float B_gyro = 0.1;       //Gyro LP filter paramter, (MPU6050 default: 0.1. MPU9250 default: 0.17)
+    float B_madgwick = 0.04;  //Madgwick filter parameter
+    const float B_accel = 0.14;     //Accelerometer LP filter paramter, (MPU6050 default: 0.14.)
+    const float B_gyro = 0.1;       //Gyro LP filter paramter, (MPU6050 default: 0.1)
     
+    float q0 = 1.0f; //Initialize quaternion for madgwick filter
+    float q1 = 0.0f;
+    float q2 = 0.0f;
+    float q3 = 0.0f;
+
     data3D Acc;
     data3D Gyro;
 
     data3D Acc_prev;
     data3D Gyro_prev;
+
+    data3D attitude;
+
+    void Madgwick6DOF(float gx, float gy, float gz, float ax, float ay, float az, float invSampleFreq);
+    float inline invSqrt(float x);
+
+    TaskHandle_t imuRead;
+    void imuReadTask( void * pvParameters);
 
 };
